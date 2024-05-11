f143d0d3de95ef28bb87e570e0f853cdbc898468
Rafael-Silva-Oliveira <rafael.slo@outlook.com>
2024-05-11 14:19:13 +0200

    Added convert_from_anndata on utils file to convert from anndata files

---
+++ scmags/_utils.py
@@ -0,0 +1,27 @@
+import scanpy as sc
+import numpy as np
+import pandas as pd
+import anndata as ad
+
+
+def convert_form_anndata(adata: ad.AnnData, cell_annotation_col: str):
+    """_summary_
+
+    Args:
+            adata (ad.AnnData): AnnData that contains .obs with the cell labels and .var with the gene names.
+            cell_annotation_col (str): Column form adata.obs that contains the cell type annotation
+
+    Returns:
+            exp_data: expression data in the form of a numpy array (n_cells x n_genes)
+            labels: 1D numpy array with the cell type labels
+            gene_names: 1D numpy array with the gene names
+    """
+
+    adata.var_names_make_unique()
+    exp_data = pd.DataFrame(
+        data=adata.X.todense(), columns=adata.var_names, index=adata.obs.index
+    ).to_numpy()
+    labels = adata.obs[cell_annotation_col].to_numpy()
+    gene_names = adata.var_names.to_numpy()
+
+    return exp_data, labels, gene_names
